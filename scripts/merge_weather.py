21c9041b38941e08ad9f001f588159ac3e9df92f
Williest <williestnyainaandry@gmail.com>
2025-06-23 09:53:43 +0300

    fix: add datetime transformation in merge_weather

--- scripts/merge_weather.py
+++ scripts/merge_weather.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
-import datetime
+from datetime import datetime
+
 
 def merge_weather(date):
     global_weather_dir = f"data/global_processed/global_weather.csv"
@@ -10,20 +11,33 @@ def merge_weather(date):
         global_df = pd.read_csv(global_weather_dir)
     else:
         global_df = pd.DataFrame()
+    global_df.rename(columns={"timezone": "timezone_hour"}, inplace=True)
 
     new_data = []
     daily_raw_dir = f"data/daily_raw/{date}"
     for csv_file in os.listdir(daily_raw_dir):
-       if csv_file.startswith("weather_") and csv_file.endswith(".csv"):
-           new_data.append(pd.read_csv(f"{daily_raw_dir}/{csv_file}"))
+        if csv_file.startswith("weather_") and csv_file.endswith(".csv"):
+            file_df = pd.read_csv(f"{daily_raw_dir}/{csv_file}")
+            city_timezone = file_df.at[0, "timezone"]
+            file_df.at[0, "sunrise"] = datetime.fromtimestamp(
+                file_df.at[0, "sunrise"] + city_timezone
+            ).strftime("%H:%M:%S")
+            file_df.at[0, "sunset"] = datetime.fromtimestamp(
+                file_df.at[0, "sunset"] + city_timezone
+            ).strftime("%H:%M:%S")
+
+            file_df.rename(columns={"timezone": "timezone_hour"}, inplace=True)
+            city_timezone_hour = city_timezone / 3600
+            file_df.at[0, "timezone_hour"] = city_timezone_hour
+
+            new_data.append(file_df)
 
     if not new_data:
         raise ValueError(f"NEW VALUES NOT FOUND FOR THE DATE: {date}")
 
     updated_df = pd.concat([global_df] + new_data, ignore_index=True)
     updated_df = updated_df.drop_duplicates(
-        subset=['city_id', 'extract_date'],
-        keep='last'
+        subset=["city_id", "extract_date"], keep="last"
     )
 
-    updated_df.to_csv(global_weather_dir, index=False) 
+    updated_df.to_csv(global_weather_dir, index=False)
